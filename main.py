5458815d324648b841a543c6c3cadccb8a618bdc
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-03-17 09:05:36 +0800

    finish real data from BMKG.go.id

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ Application Detection Latest Earthquake
 import gempaterkini
 
 if __name__ == '__main__':
-    print('First application')
+    print('My application')
     result = gempaterkini.extraction_data()
     gempaterkini.display_data(result)
