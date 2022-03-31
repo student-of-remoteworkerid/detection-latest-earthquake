df7160f68a232c55511f77905ab651f65727ef86
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-04-01 03:36:56 +0800

    run with procedural function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
 Application Detection Latest Earthquake
+Modularization with function
+Modularization with Package
 """
 import earthquakelatest
 
 if __name__ == '__main__':
-    print('My application')
+    print(f'My application using package have description {earthquakelatest.description}')
     result = earthquakelatest.extraction_data()
     earthquakelatest.display_data(result)
