e13990b270a6aadee97c83520617720c75ab2acc
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-04-01 03:59:48 +0800

    final version

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Modularization with Package
 import earthquakelatest
 
 if __name__ == '__main__':
-    print(f'My application using package have description {earthquakelatest.description}')
-    result = earthquakelatest.extraction_data()
-    earthquakelatest.display_data(result)
+    earthquake_in_indonesian = earthquakelatest.LatestEarthquake('https://bmkg.go.id')
+    print(f'My application using package have description {earthquake_in_indonesian.description}')
+    earthquake_in_indonesian.display_description()
+    earthquake_in_indonesian.run()
